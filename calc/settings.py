5b303452b04199e3314d4962708edab70ce5dfdb
victor <seregi4ev@gmail.com>
2017-12-14 22:57:27 +0300

    Добавлены сметы с добавление товаров в смету

--- calc/settings.py
+++ calc/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'calc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'static/../db.sqlite3'),
     }
 }
 
